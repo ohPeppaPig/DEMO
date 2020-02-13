152127769b9b74524673d8299cce6432b0d69c9b
MR.zhu <852475584@qq.com>
2020-02-13 20:38:02 +0800

    第三次修改

--- src/demo/test.java
+++ src/demo/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         Employee emp = new Employee();
         emp.setAge(11);
-        emp.setName("李四");
+        emp.setName("王五");
         emp.setSex("woman");
         System.out.println(emp.toString());
         System.out.println("hello");
